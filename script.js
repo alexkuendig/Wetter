b0dcc21fd4cf7f975b29274390c42746a26c41db
Alex Kündig <alexkuendig@bluewin.ch>
2015-06-05 11:12:46 +0200

    Daten geo anzeigen

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
 	navigator.geolocation.getCurrentPosition(function(position){
 		console.log(position);
-	
+	$('.latitude').text(position.coords.latitude);
+	$('.longitude').text(position.coords.longitude);
+	$('.accuracy').text(position.coords.accuracy);
 
 		});
 
