7b8bf6f495cdfa13f8e0dee6c9893850d675ae40
Alex Kündig <alexkuendig@bluewin.ch>
2015-06-12 10:09:52 +0200

    temperatur

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ $(document).ready(function(){
 	$('.latitude').text(position.coords.latitude);
 	$('.longitude').text(position.coords.longitude);
 	$('.accuracy').text(position.coords.accuracy);*/
+	$('.temperatur').text(position.currently.apparentTemperature);
 
 	});
 
