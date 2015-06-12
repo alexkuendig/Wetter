0fa19cdf8deb449da13ff746d2ec571d068d4214
Alex Kündig <alexkuendig@bluewin.ch>
2015-06-12 09:03:12 +0200

    koordinaten hinzugefuegt

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 $(document).ready(function(){
 	navigator.geolocation.getCurrentPosition(function(position){
-		console.log(position);
+
+		var koordinaten = {
+			latitude: 	position.coords.latitude;
+			longitude: 	position.coords.longitude;
+		};
+
+		$.ajax(){
+			url: 'https://api.forecast.io/forecast/1b227f5bfda2a75058c33b2f109c7b0a/'+ koordinaten.latitude + ',' +koordinaten.longitude,
+			data: {
+				units: 'ca',
+				lang: 'de'
+			},
+			dataType: 'jasop'
+			).done(function(data){
+				console.log(data);
+
+
+		});
+
+		/*console.log(position);
 	$('.latitude').text(position.coords.latitude);
 	$('.longitude').text(position.coords.longitude);
-	$('.accuracy').text(position.coords.accuracy);
+	$('.accuracy').text(position.coords.accuracy);*/
 
 		});
 
